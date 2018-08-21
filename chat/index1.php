b69acc572c3ddc6f87b7760dcb84c61b306d48e0
Chengyang Li <33701447+edwardli9908@users.noreply.github.com>
2018-08-21 00:24:57 -0400

    remove useless files

--- chat/index1.php
+++
@@ -1,4 +0,0 @@
-<?php
-// This file has been declared as deprecated. It stays here only for old bookmarks users might have pointing to this file
-require("index.php");
-?>
--- chat/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$_SESSION["adminlogged"] = NULL;
-	unset($_SESSION["adminlogged"]);
-	$_SESSION = array();
-?>
