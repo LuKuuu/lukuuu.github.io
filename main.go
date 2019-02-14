6b97b017b802bf54538f17e15ee59ec324c548a7
Edward Li <chengli@blackberry.com>
2019-02-14 17:44:38 -0500

    add read port from file

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -19,11 +21,32 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type config struct {
+	Port string
+}
+
+func readPortFromFile() string {
+	dat, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	conf := config{Port: ":80"}
+	err = json.Unmarshal(dat, &conf)
+	if err != nil {
+		panic(err)
+	}
+
+	return conf.Port
+
+}
+
 func main() {
 	fmt.Printf("starting...\n")
+	port := readPortFromFile()
 	http.HandleFunc("/", homepage)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		fmt.Printf("%v", err)
 	}
